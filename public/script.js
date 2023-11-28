2b891a6b97a5227ae65133b1d0e4499fc7521c73
Your Name <you@example.com>
2023-11-28 14:43:55 +0000

    site publish changes

---
+++ public/script.js
@@ -0,0 +1,17 @@
+
+console.log("Script is loaded!");
+
+
+document.addEventListener("DOMContentLoaded", function () 
+{
+    // Select the toggle button and the dropdown menu
+    const toggleBtn = document.querySelector(".toggle-button");
+    const dropdown = document.querySelector(".dropdown-menu");
+  
+    // Add a click event listener to the toggle button
+    toggleBtn.addEventListener("click", () => {
+      // Toggle the 'top-16' class on the dropdown menu
+      dropdown.classList.toggle('top-16');
+    });
+  });
+  
