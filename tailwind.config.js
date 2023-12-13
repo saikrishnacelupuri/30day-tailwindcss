829698b02b5c64d8dd08c91e39d0c3f4ee58f203
krishnacelupuri <krishnacelupuri@gmail.com>
2023-12-13 06:04:32 +0000

    changes to live

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,114 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        primary: ["Inter", "sans-serif"],
+        system: [
+          "-apple-system",
+          "BlinkMacSystemFont",
+          '"Segoe UI"',
+          "Roboto",
+          '"Helvetica Neue"',
+          "Arial",
+          "sans-serif",
+        ],
+      },
+      fontWeight: {
+        normal: 400,
+        medium: 400,
+        semibold: 600,
+        bold: 700,
+      },
+      fontSize: {
+        "hero-title": [
+          "var(--fontWeightMedium)",
+          "34px",
+          "44px",
+          "var(--fontFamily)",
+        ],
+        "section-title": [
+          "var(--fontWeightMedium)",
+          "24px",
+          "34px",
+          "var(--fontFamily)",
+        ],
+        "subsection-title": [
+          "var(--fontWeightSemibold)",
+          "20px",
+          "28px",
+          "var(--fontFamily)",
+        ],
+        "detail-title": [
+          "var(--fontWeightSemibold)",
+          "16px",
+          "24px",
+          "var(--fontFamily)",
+        ],
+        "hero-body": [
+          "var(--fontWeightNormal)",
+          "16px",
+          "26px",
+          "var(--fontFamily)",
+        ],
+        "section-body": [
+          "var(--fontWeightNormal)",
+          "16px",
+          "26px",
+          "var(--fontFamily)",
+        ],
+        "subsection-body": [
+          "var(--fontWeightNormal)",
+          "16px",
+          "26px",
+          "var(--fontFamily)",
+        ],
+        "detail-body": [
+          "var(--fontWeightNormal)",
+          "14px",
+          "24px",
+          "var(--fontFamily)",
+        ],
+        cta: ["var(--fontWeightSemibold)", "15px", "15px", "var(--fontFamily)"],
+        link: [
+          "var(--fontWeightSemibold)",
+          "14px",
+          "14px",
+          "var(--fontFamily)",
+        ],
+      },
+      colors: {
+        white: "#fefefe",
+        greyLight: "#abb5ba",
+        greyDark: "#1d3944",
+        greyDarker: "#14262e",
+        teal: "#33ddb3",
+        tealLight: "#e3fff2",
+        tealDark: "#2dc49f",
+        tealDarker: "#18c59a",
+        tealDarkest: "#05a87f",
+        blurple: "#635bff",
+      },
+      spacing: {
+        columnGap: "24px",
+        gutter: "16px",
+        maxColumnWidth: "68px",
+        maxColumnCount: "12",
+        columnCount: "4",
+        maxWidth:
+          "calc(var(--layoutMaxColumnWidth) * var(--layoutMaxColumnCount) + var(--layoutColumnGap) * (var(--layoutMaxColumnCount) - 1) + var(--layoutGutter) * 2)",
+        copyColumnCountMax: "10",
+        copyMaxWidth:
+          "calc(var(--layoutMaxColumnWidth) * var(--copyColumnCountMax) + var(--layoutColumnGap) * (var(--copyColumnCountMax) - 1))",
+      },
+      transitionDuration: {
+        hover: "75ms",
+      },
+      opacity: {
+        linkHover: "0.6",
+        buttonHover: "0.8",
+      },
+    },
   },
   plugins: [],
-}
-
+};
