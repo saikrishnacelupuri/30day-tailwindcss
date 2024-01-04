df51fef99ae6e84d5cc18a34aa630f685b2a0fa9
krishnacelupuri <krishnacelupuri@gmail.com>
2024-01-04 14:38:45 +0000

    changes

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-
 console.log("Script is loaded!");
 
+document.addEventListener("DOMContentLoaded", function () {
+  // Select the toggle button and the dropdown menu
+  const toggleBtn = document.querySelector(".toggle-button");
+  const dropdown = document.querySelector(".dropdown-menu");
 
-document.addEventListener("DOMContentLoaded", function () 
-{
-    // Select the toggle button and the dropdown menu
-    const toggleBtn = document.querySelector(".toggle-button");
-    const dropdown = document.querySelector(".dropdown-menu");
-  
-    // Add a click event listener to the toggle button
-   toggleBtn.addEventListener("click", () => {
-
-
-console.log("Sclick works!");
-      // Toggle the 'top-16' class on the dropdown menu
-      dropdown.classList.toggle('top-16');
-    });
+  // Add a click event listener to the toggle button
+  toggleBtn.addEventListener("click", () => {
+    console.log("Sclick works!");
+    // Toggle the 'top-16' class on the dropdown menu
+    dropdown.classList.toggle("top-16");
   });
-  
+});
